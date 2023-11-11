4202ac86f99a7da972f760dbca963d265cf6ec27
John Cruz <jcruz@flexion.us>
2023-11-11 15:45:15 -0700

    rename method

--- services/runPDFTest.js
+++ services/runPDFTest.js
@@ -2,7 +2,7 @@ function runPDFTest(
     async, execute
 ) {
 
-    function runPDFTest(options, callback) {
+    function runTests(options, callback) {
         const command = 'docker build -t efcms -f Dockerfile . && docker build -t efcms-local -f Dockerfile-local . && docker run -it --rm -v `pwd`/shared/test-output:/home/app/shared/test-output efcms-local sh -c "npm run test:document-generation"';
         const commandOptions = { cwd: options.PATH_TO_REPO };
         execute(command, commandOptions, (error) => callback(error));
@@ -10,7 +10,7 @@ function runPDFTest(
 
     return function (options, callback) {
         const TASKS = [
-            (continuation) => runPDFTest(options, continuation)
+            (continuation) => runTests(options, continuation)
         ];
         async.waterfall(TASKS, () => {
             if (error) return console.log(`The PDF tests failed, please manually check what is wrong\nErrors shown below\n${error}`);
