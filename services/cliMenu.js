65552d61e0151cf5b0251c69b860c9281d559ae1
John Cruz <jcruz@flexion.us>
2023-11-14 09:13:03 -0700

    Reorganize

--- services/cliMenu.js
+++ services/cliMenu.js
@@ -1,70 +1,70 @@
 function cliMenu(
-    inquirer,
+  inquirer,
+  updateMinorVersionsMenu,
+  updateMajorVersionsMenu,
+  runPackagesAuditMenu,
+  updateDockerTerraformVersionMenu,
+  updateDockerAwsCliVersionMenu,
+  updateCypressBaseImageMenu,
+  deployNewECRImagesToFlexionAndUstcEnvMenu,
+  updateAWSProviderForTerraformMenu,
+  runPDFTestMenu,
+  runMigrationToExperimentalEnvMenu
+) {
+  const availableSteps = [
     updateMinorVersionsMenu,
     updateMajorVersionsMenu,
     runPackagesAuditMenu,
     updateDockerTerraformVersionMenu,
     updateDockerAwsCliVersionMenu,
     updateCypressBaseImageMenu,
-    deployNewECRImagesToFlexionAndUstcEnvMenu,
     updateAWSProviderForTerraformMenu,
     runPDFTestMenu,
+    deployNewECRImagesToFlexionAndUstcEnvMenu,
     runMigrationToExperimentalEnvMenu,
-) {
-    const availableSteps = [
-        updateMinorVersionsMenu,
-        updateMajorVersionsMenu,
-        runPackagesAuditMenu,
-        updateDockerTerraformVersionMenu,
-        updateDockerAwsCliVersionMenu,
-        updateCypressBaseImageMenu,
-        deployNewECRImagesToFlexionAndUstcEnvMenu,
-        updateAWSProviderForTerraformMenu,
-        runPDFTestMenu,
-        runMigrationToExperimentalEnvMenu,
+  ];
+
+  function executeOption(options, { title, description, method }) {
+    console.clear();
+    console.log("Step: ", title);
+    console.log("Description: ", description);
+
+    const KEY = "RUN_METHOD";
+    const QUESTION = [
+      {
+        type: "confirm",
+        name: KEY,
+        message: "Run step?",
+        default: true,
+      },
     ];
 
-    function executeOption(options, { title, description, method }) {
-        console.clear();
-        console.log('Step: ', title);
-        console.log('Description: ', description);
+    inquirer.prompt(QUESTION).then((answers) => {
+      if (!answers[KEY]) return startMenu();
+      method(options, () => startMenu(options));
+    });
+  }
 
-        const KEY = 'RUN_METHOD';
-        const QUESTION = [
-            {
-                type: 'confirm',
-                name: KEY,
-                message: 'Run step?',
-                default: true,
-            },
-        ];
+  function startMenu(options) {
+    console.clear();
+    const QUESTION = [
+      {
+        type: "rawlist",
+        name: "option",
+        message: `What would you want to do?`,
+        choices: availableSteps.map((x) => x.title),
+      },
+    ];
 
-        inquirer
-            .prompt(QUESTION)
-            .then((answers) => {
-                if (!answers[KEY]) return startMenu();
-                method(options, () => startMenu());
-            })
-    }
+    inquirer.prompt(QUESTION).then((answers) => {
+      const step = availableSteps.find(
+        (currentStep) => currentStep.title === answers.option
+      );
+      executeOption(options, step);
+    });
+  }
 
-    function startMenu(options) {
-        console.clear()
-        const QUESTION = [{
-            type: "rawlist",
-            name: "option",
-            message: `What would you want to do?`,
-            choices: availableSteps.map(x => x.title),
-        }];
-
-        inquirer
-            .prompt(QUESTION)
-            .then((answers) => {
-                const step = availableSteps.find((currentStep) => currentStep.title === answers.option);
-                executeOption(options, step);
-            });
-    }
-
-    return startMenu;
+  return startMenu;
 }
 
 module.exports = cliMenu;
